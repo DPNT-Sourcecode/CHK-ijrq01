cfe0895d74126fc2c6c2d2f05162cf4435b0c743
sbx_user1059 <sbx_user1059@ip-10-11-232-98.eu-west-2.compute.internal>
2018-02-23 16:21:37 +0000

    Fri Feb 23 16:21:37 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    price_table = [
+        {
+            'item': 'A',
+            'price': 50,
+            'offers': [
+                '3A for 130',
+            ],
+        },
+        {
+            'item': 'B',
+            'price': 30,
+            'offers': [
+                '2B for 45',
+            ],
+        },
+        {
+            'item': 'C',
+            'price': 20,
+            'offers': [],
+        },
+        {
+            'item': 'D',
+            'price': 15,
+            'offers': [],
+        },
+    ]
